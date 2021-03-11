515dfec3b82641dacee3e74fa76689a37c897d3d
Greg Roberts <groberts3104@gmail.com>
2021-03-11 13:55:59 -0500

    added bootstrap and Name component

--- client/src/pages/Form/index.js
+++ client/src/pages/Form/index.js
@@ -7,6 +7,10 @@ function Form() {
       <h1 className="header">
         <span>Roberts Form #1</span>
       </h1>
+      <div className="form-group">
+  <label for="usr">Name:</label>
+  {/* <input type="text" className="form-control" id="usr"> */}
+</div>
     </div>
   );
 }
